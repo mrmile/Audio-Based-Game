72753e6ca05a6a98d1a8c3bc6c1fde627533cdcd
mrmile <73582929+mrmile@users.noreply.github.com>
2024-02-25 00:34:47 +0100

    Finished circle obstacle type

---
+++ Assets/Scripts/ObstacleSpawners/Circle.cs
@@ -0,0 +1,123 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Circle : MonoBehaviour
+{
+    LevelsManager level_;
+    R_Easings easings_;
+
+    public GameObject obstacle;
+    public GameObject obstacleWarning;
+
+    public Vector2 minPosXY;
+    public Vector2 maxPosXY;
+    public Vector2 scale;
+
+    public float rotationSpeed = 0;
+    public float livingTime = 0;
+    public float warningTime = 0;
+    public float shakeIntensity = 0;
+
+    public string dontDestroyAtEndIfParentNameIs = "_null_";
+    private bool dontDestroyAtEnd = false; //turn it only true if used in a snake
+
+    private float startTime = 0;
+    private float obstacleTime = 0;
+
+    private int step = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        level_ = GetComponentInParent<LevelsManager>();
+        easings_ = GetComponent<R_Easings>();
+
+        obstacleWarning.transform.localScale = new Vector3(0, 0, 0);
+        obstacle.transform.localScale = new Vector3(0, 0, 0);
+
+        obstacle.transform.localPosition = new Vector3(Random.Range(minPosXY.x, maxPosXY.x), Random.Range(minPosXY.y, maxPosXY.y), 0);
+        obstacleWarning.transform.localPosition = new Vector3(Random.Range(minPosXY.x, maxPosXY.x), Random.Range(minPosXY.y, maxPosXY.y), 0);
+
+        if (gameObject.transform.parent != null)
+        {
+            if (gameObject.transform.parent.name == dontDestroyAtEndIfParentNameIs)
+            {
+                dontDestroyAtEnd = true;
+            }
+        }
+
+        startTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        obstacleTime = Time.time - startTime;
+
+        if(rotationSpeed != 0)
+        {
+            obstacleWarning.transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
+        }
+
+        if (obstacleWarning.transform.localScale.x < scale.x)
+        {
+            obstacleWarning.transform.localScale = new Vector3(scale.x, scale.y, 0);
+        }
+        else if (step == 0 && obstacleTime > warningTime)
+        {
+            if (shakeIntensity != 0)
+            {
+                level_.ShakeCamera(0.1f, shakeIntensity);
+            }
+
+            step++;
+            startTime = Time.time;
+            obstacleTime = Time.time - startTime;
+        }
+
+        if (obstacleTime < 0.03f && step == 1)
+        {
+            obstacle.transform.localScale = new Vector3(easings_.EaseLinearNone(obstacleTime, 0, scale.x - 0, 0.03f), easings_.EaseLinearNone(obstacleTime, 0, scale.y - 0, 0.03f), 0);
+        }
+        else if (step == 1)
+        {
+            //obstacleWarning.transform.localScale = new Vector3(0, 20, 0);
+            
+            step++;
+            startTime = Time.time;
+            obstacleTime = Time.time - startTime;
+        }
+
+        if (step == 2 && obstacleTime > livingTime)
+        {
+            //obstacleWarning.transform.localScale = new Vector3(0, 20, 0);
+            step++;
+            startTime = Time.time;
+            obstacleTime = Time.time - startTime;
+        }
+
+        if (obstacle.transform.localScale.x > 0 && step == 3)
+        {
+            obstacle.transform.localScale = new Vector3(easings_.EaseLinearNone(obstacleTime, scale.x, 0 - scale.x, 0.03f), easings_.EaseLinearNone(obstacleTime, scale.y, 0 - scale.y, 0.03f), 0);
+            obstacleWarning.transform.localScale = new Vector3(easings_.EaseLinearNone(obstacleTime, scale.x, 0 - scale.x, 0.03f), easings_.EaseLinearNone(obstacleTime, scale.y, 0 - scale.y, 0.03f), 0);
+        }
+        else if (step == 3)
+        {
+            step++;
+            startTime = Time.time;
+            obstacleTime = Time.time - startTime;
+        }
+
+        if (step == 4 && dontDestroyAtEnd == false)
+        {
+            Destroy(obstacleWarning);
+            Destroy(obstacle);
+            Destroy(gameObject);
+        }
+        else if (step == 4 && dontDestroyAtEnd == true)
+        {
+            gameObject.transform.position = new Vector3(99999, 99999, 99999);
+        }
+    }
+}
